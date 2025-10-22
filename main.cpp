854f2228eed65ea1031908e89eba2ed39b4c63bf
book <shakhorinnikolay@gmail.com>
2025-10-22 10:43:59 +0300

    Добавлено удаление студента(не протестировано)

--- main.cpp
+++ main.cpp
@@ -8,6 +8,7 @@
 #include <iomanip>
 #include <memory>
 #include <sstream>
+#include <regex>
 
 using namespace std;
 
@@ -36,29 +37,42 @@ int loadChekUserData(string login,string password);
 void userMenu();
 void adminMenu();
 void loadFromTextFile();
+void loadFromKeyboard();
 void parsingText(vector<string>);
 void fixStreamState();
 int getCorrectValue();
+bool freeIdCheck();
+int getCorrectMark();
+int getCorrectLevel();
+int getCorrectFirstYear();
+int getCorrectId();
+char getCorrectChar();
+string getCorrectFIO();
+string getCorrectGroup();
+void studentsWithBadMarksChek();
 // Функции взаимодействия с базой
 void addNewStudent(vector<string>);
 void printAllDataFromBase();
+void printStudentsWithBadMarks(vector<int>);
+void delStudentFromBase();
 // Конец область функций
 
 int main()
 {
     system("chcp 65001");
     getLoginPass();
-    int userChoice{};
     switch (UserType)
     {
     case 1:
-        // Пользователь
-        do
         {
-            userMenu();
-            int userChoice = getCorrectValue();
-            switch (userChoice)
+            // Пользователь
+            int userChoice{};
+            do
             {
+                userMenu();
+                userChoice = getCorrectValue();
+                switch (userChoice)
+                {
                 case 1:
                     loadFromTextFile();
                     break;
@@ -68,6 +82,7 @@ int main()
                     printAllDataFromBase();
                     break;
                 case 4:
+                    studentsWithBadMarksChek();
                     break;
                 case 5:
                     break;
@@ -79,48 +94,54 @@ int main()
                     break;
                 default:
                     cout << "Введен неверный номер" << endl;
-            }
-        }while(userChoice != 8);
-        break;
+                }
+            }while(userChoice != 8);
+            break;
+        }
     case 2:
-        // Администратор
-        do
         {
-            adminMenu();
-            int userChoice = getCorrectValue();
-            switch (userChoice)
+            // Администратор
+            int adminChoice{};
+            do
             {
-            case 1:
-                break;
-            case 2:
-                loadFromTextFile();
-                break;
-            case 3:
-                break;
-            case 4:
-                printAllDataFromBase();
-                break;
-            case 5:
-                break;
-            case 6:
-                break;
-            case 7:
-                break;
-            case 8:
-                break;
-            case 9:
-                break;
-            case 10:
-                break;
-            case 11:
-                break;
-            case 12:
-                break;
-            default:
-                cout << "Введен неверный номер" << endl;
-            }
-        }while(userChoice != 12);
-        break;
+                adminMenu();
+                adminChoice = getCorrectValue();
+                switch (adminChoice)
+                {
+                case 1:
+                    loadFromKeyboard();
+                    break;
+                case 2:
+                    loadFromTextFile();
+                    break;
+                case 3:
+                    break;
+                case 4:
+                    printAllDataFromBase();
+                    break;
+                case 5:
+                    studentsWithBadMarksChek();
+                    break;
+                case 6:
+                    break;
+                case 7:
+                    break;
+                case 8:
+                    break;
+                case 9:
+                    break;
+                case 10:
+                    break;
+                case 11:
+                    break;
+                case 12:
+                    break;
+                default:
+                    cout << "Введен неверный номер" << endl;
+                }
+            }while(adminChoice != 12);
+            break;
+        }
     default:
         cout << "Ошибка авторизации" << endl;
     }
@@ -129,66 +150,8 @@ int main()
 }
 
 // Область реализации функций
-void adminMenu()
-{
-    cout <<"\n";
-    cout << "========== СИСТЕМА УПРАВЛЕНИЯ СТУДЕНТАМИ ==========\n" << endl;
-    cout << "1. Ввод информации с клавиатуры" << endl;
-    cout << "2. Загрузка из текстового файла" << endl;
-    cout << "3. Загрузка из бинарного файла" << endl;
-    cout << "4. Вывод данных в виде таблицы" << endl;
-    cout << "5. Выполнение запроса (поиск двоечников)" << endl;
-    cout << "6. Экспорт в текстовый файл" << endl;
-    cout << "7. Конвертация в бинарный файл" << endl;
-    cout << "8. Добавление записи" << endl;
-    cout << "9. Изменение записи" << endl;
-    cout << "10. Удаление записи" << endl;
-    cout << "11. Сортировка данных" << endl;
-    cout << "12. Выход" << endl;
-    cout << "\nВведите номер команды: ";
-}
-void printAllDataFromBase()
-{
-    bool arrayIsEmpty = FreeId.size() == 10;
-    if (arrayIsEmpty)
-    {
-        cout << "База пуста" << endl;
-        return;
-    }
-    cout << string(42, ' ');
-    cout << "===== СТУДЕНТЫ =====" << endl;
-    cout << endl;
 
-    // Заголовок таблицы
-    cout << " | ";
-    cout << setw(4) << "ID" << " | ";
-    cout << setw(12) << "     Фамилия" << " | ";
-    cout << setw(12) << "         Имя" << " | ";
-    cout << setw(12) << "    Отчество" << " | ";
-    cout << setw(4) << "Курс" << " | ";
-    cout << setw(10) << "    Группа" << " | ";
-    cout << setw(15) << "Год поступления" << " | ";
-    cout << setw(10) << "   Оценки" << " | " << endl;
-
-    // Разделитель
-    cout << string(104, '=') << endl;
-
-    // Данные студентов
-    for (Student student: Students)
-    {
-        if (student.id == 0) continue;
-        cout << string(104, '-') << endl;
-        cout << setw(3) << " | ";
-        cout << setw(4) << student.id << " | ";
-        cout << setw(12) << student.lastname << " | ";
-        cout << setw(12) << student.name << " | ";
-        cout << setw(12) << student.fathername << " | ";
-        cout << setw(4) << student.level << " | ";
-        cout << setw(10) << student.group << " | ";
-        cout << setw(15) << student.firstYear << " | ";
-        cout << student.marks[0] << ";" << student.marks[1] << ";" << student.marks[2] << ";" << student.marks[3] << ";" << student.marks[4] << " | " << endl;
-    }
-}
+// Функции авторизации
 int getLoginPass()
 {
     string login{},password{};
@@ -294,6 +257,25 @@ int loadChekUserData(string login, string password)
     if (UserType != 1 and UserType != 2) UserType = 0;
     return UserType;
 }
+// Функции меню
+void adminMenu()
+{
+    cout <<"\n";
+    cout << "========== СИСТЕМА УПРАВЛЕНИЯ СТУДЕНТАМИ ==========\n" << endl;
+    cout << "1. Ввод информации с клавиатуры" << endl;
+    cout << "2. Загрузка из текстового файла" << endl;
+    cout << "3. Загрузка из бинарного файла" << endl;
+    cout << "4. Вывод данных в виде таблицы" << endl;
+    cout << "5. Выполнение запроса (поиск двоечников)" << endl;
+    cout << "6. Экспорт в текстовый файл" << endl;
+    cout << "7. Конвертация в бинарный файл" << endl;
+    cout << "8. Добавление записи" << endl;
+    cout << "9. Изменение записи" << endl;
+    cout << "10. Удаление записи" << endl;
+    cout << "11. Сортировка данных" << endl;
+    cout << "12. Выход" << endl;
+    cout << "\nВведите номер команды: ";
+}
 void userMenu()
 {
     cout <<"\n";
@@ -308,6 +290,7 @@ void userMenu()
     cout << "8. Выход" << endl;
     cout << "\nВведите номер команды: ";
 }
+// Функции ввода
 void loadFromTextFile()
 {
     vector<string> txtLines{};
@@ -347,32 +330,53 @@ void loadFromTextFile()
         cout << "Ошибка! Неверный формат." << endl;
     }
 }
-void parsingText(vector<string> txtLines)
+void loadFromKeyboard()
 {
-    vector<string> dataOneStudent{};
-    int indexString{};
-    for (string txtString :txtLines)
+    if (!freeIdCheck())
     {
-        vector<string> dataOneStudent{};
-        if (indexString++ == 0) continue;
-        stringstream ss(txtString);
-        string word{};
-        while (getline(ss, word, ';'))
-        {
-            if (!word.empty()) {
-                dataOneStudent.push_back(word);
-            }
-        }
-        if (size(dataOneStudent) != 11)
-        {
-            cout << "Неверно отформатирована строка:" << indexString - 1 << endl;
-            cout << txtString << endl;
-            continue;
-        }
-
-        addNewStudent(dataOneStudent);
+        cout << "В базу занесено максимальное количество студентов" << endl;
+        return;
     }
+    vector<string> dataOneStudent{};
+    cout << "Введите фамилию" << endl;
+    string lastname{};
+    lastname = getCorrectFIO();
+    dataOneStudent.push_back(lastname);
+    cout << "Введите имя" << endl;
+    string name{};
+    name = getCorrectFIO();
+    dataOneStudent.push_back(name);
+    cout << "Введите отчество" << endl;
+    string fathername{};
+    fathername = getCorrectFIO();
+    dataOneStudent.push_back(fathername);
+    cout << "Введите курс" << endl;
+    int level_i{};
+    string level{};
+    level_i = getCorrectLevel();
+    level = to_string(level_i);
+    dataOneStudent.push_back(level);
+    cout << "Введите группу" << endl;
+    string group{};
+    group = getCorrectGroup();
+    dataOneStudent.push_back(group);
+    cout << "Введите год поступления" << endl;
+    string firstYear{};
+    int firstYear_i{};
+    firstYear_i = getCorrectFirstYear();
+    firstYear = to_string(firstYear_i);
+    dataOneStudent.push_back(firstYear);
+    cout << "Введите 5 оценок" << endl;
+    int marks[5]{};
+    for (int i = 0; i < 5; i++)
+    {
+        marks[i] = getCorrectMark();
+        string mark = to_string(marks[i]);
+        dataOneStudent.push_back(mark);
+    }
+    addNewStudent(dataOneStudent);
 }
+// Функции взаибодействия с массивом
 void addNewStudent(vector<string> dataOneStudent)
 {
     if (dataOneStudent.size() != 11)
@@ -412,6 +416,146 @@ void addNewStudent(vector<string> dataOneStudent)
         cout << "Данные в файле в неверном формате" << endl;
     }
 }
+void delStudentFromBase()
+{
+    char accept{};
+    int idForDel{};
+    bool flagExit{false};
+    do{
+        if (flagExit)
+        {
+            char exitChar{};
+            cout << "Вы все ещё хотите удалить какого-либо студента?(y/n)" << endl;
+            exitChar = getCorrectChar();
+            if (exitChar == 'n' or exitChar == 'N')
+            {
+                return;
+            }
+        }
+        printAllDataFromBase();
+        cout << "Введите ID студента для удаления" << endl;
+        idForDel = getCorrectId();
+        cout << "Вы действительно хотите удалить студента с ID:" << idForDel << "?(y/n)" << endl;
+        accept = getCorrectChar();
+        flagExit = true;
+    }while (accept == 'n' or accept == 'N');
+    int indexStudentForDel{};
+    for (Student student : Students)
+    {
+        if (student.id == idForDel)
+        {
+            Student newStudent{};
+            Students[indexStudentForDel] = newStudent;
+            break;
+        }
+        indexStudentForDel++;
+    }
+}
+// Функции вывода
+void printAllDataFromBase()
+{
+    bool arrayIsEmpty = FreeId.size() == 10;
+    if (arrayIsEmpty)
+    {
+        cout << "База пуста" << endl;
+        return;
+    }
+    cout << string(42, ' ');
+    cout << "===== СТУДЕНТЫ =====" << endl;
+    cout << endl;
+
+    // Заголовок таблицы
+    cout << " | ";
+    cout << setw(4) << "ID" << " | ";
+    cout << setw(12) << "     Фамилия" << " | ";
+    cout << setw(12) << "         Имя" << " | ";
+    cout << setw(12) << "    Отчество" << " | ";
+    cout << setw(4) << "Курс" << " | ";
+    cout << setw(10) << "    Группа" << " | ";
+    cout << setw(15) << "Год поступления" << " | ";
+    cout << setw(10) << "   Оценки" << " | " << endl;
+
+    // Разделитель
+    cout << string(104, '=') << endl;
+
+    // Данные студентов
+    for (Student student: Students)
+    {
+        if (student.id == 0) continue;
+        cout << string(104, '-') << endl;
+        cout << setw(3) << " | ";
+        cout << setw(4) << student.id << " | ";
+        cout << setw(12) << student.lastname << " | ";
+        cout << setw(12) << student.name << " | ";
+        cout << setw(12) << student.fathername << " | ";
+        cout << setw(4) << student.level << " | ";
+        cout << setw(10) << student.group << " | ";
+        cout << setw(15) << student.firstYear << " | ";
+        cout << student.marks[0] << ";" << student.marks[1] << ";" << student.marks[2] << ";" << student.marks[3] << ";" << student.marks[4] << " | " << endl;
+    }
+}
+void printStudentsWithBadMarks(vector<int> idStudentsWithBadMarks)
+{
+    cout << string(42, ' ');
+    cout << "===== СТУДЕНТЫ =====" << endl;
+    cout << endl;
+
+    // Заголовок таблицы
+    cout << " | ";
+    cout << setw(4) << "ID" << " | ";
+    cout << setw(12) << "     Фамилия" << " | ";
+    cout << setw(12) << "         Имя" << " | ";
+    cout << setw(12) << "    Отчество" << " | ";
+    cout << setw(4) << "Курс" << " | ";
+    cout << setw(10) << "    Группа" << " | ";
+    cout << setw(15) << "Год поступления" << " | ";
+    cout << setw(10) << "   Оценки" << " | " << endl;
+
+    // Разделитель
+    cout << string(104, '=') << endl;
+
+    // Данные студентов
+    for (Student student : Students){
+        if (count(idStudentsWithBadMarks.begin(),idStudentsWithBadMarks.end(),student.id) == 0) continue;
+        cout << string(104, '-') << endl;
+        cout << setw(3) << " | ";
+        cout << setw(4) << student.id << " | ";
+        cout << setw(12) << student.lastname << " | ";
+        cout << setw(12) << student.name << " | ";
+        cout << setw(12) << student.fathername << " | ";
+        cout << setw(4) << student.level << " | ";
+        cout << setw(10) << student.group << " | ";
+        cout << setw(15) << student.firstYear << " | ";
+        cout << student.marks[0] << ";" << student.marks[1] << ";" << student.marks[2] << ";" << student.marks[3] << ";" << student.marks[4] << " | " << endl;
+    }
+}
+// Функции прочие
+void parsingText(vector<string> txtLines)
+{
+    vector<string> dataOneStudent{};
+    int indexString{};
+    for (string txtString :txtLines)
+    {
+        vector<string> dataOneStudent{};
+        if (indexString++ == 0) continue;
+        stringstream ss(txtString);
+        string word{};
+        while (getline(ss, word, ';'))
+        {
+            if (!word.empty()) {
+                dataOneStudent.push_back(word);
+            }
+        }
+        if (size(dataOneStudent) != 11)
+        {
+            cout << "Неверно отформатирована строка:" << indexString - 1 << endl;
+            cout << txtString << endl;
+            continue;
+        }
+
+        addNewStudent(dataOneStudent);
+    }
+}
 void fixStreamState() {
     cin.clear();
     cin.ignore(numeric_limits<streamsize>::max(), '\n');
@@ -422,6 +566,7 @@ int getCorrectValue() {
 
     do {
         isNotOk = false;
+        regex pattern("[^0-9]");
         if ((cin >> n).fail()) {
             fixStreamState();
             cout << "Неверный тип данных!" << endl;
@@ -432,3 +577,166 @@ int getCorrectValue() {
 
     return n;
 }
+bool freeIdCheck()
+{
+    if (FreeId.size() > 0)
+    {
+        return true;
+    }else
+    {
+        return false;
+    }
+}
+int getCorrectMark()
+{
+    int n{};
+    bool isNotOk{};
+
+    do {
+        isNotOk = false;
+        if ((cin >> n).fail() or (n < 2 or n > 5)) {
+            fixStreamState();
+            cout << "Неверное значение!" << endl;
+            cout << "Введите оценку: " << endl;
+            isNotOk = true;
+        }
+    } while (isNotOk);
+
+    return n;
+}
+int getCorrectLevel()
+{
+    int n{};
+    bool isNotOk{};
+
+    do {
+        isNotOk = false;
+        if ((cin >> n).fail() or (n < 1 or n > 6)) {
+            fixStreamState();
+            cout << "Неверное значение!" << endl;
+            cout << "Введите курс: " << endl;
+            isNotOk = true;
+        }
+    } while (isNotOk);
+
+    return n;
+}
+int getCorrectFirstYear()
+{
+    int n{};
+    bool isNotOk{};
+
+    do {
+        isNotOk = false;
+        if ((cin >> n).fail() or (n < 2000 or n > 2025)) {
+            fixStreamState();
+            cout << "Неверное значение!" << endl;
+            cout << "Введите год в промежутке от 2000 до 2025: " << endl;
+            isNotOk = true;
+        }
+    } while (isNotOk);
+
+    return n;
+}
+int getCorrectId()
+{
+    int n{};
+    bool isNotOk{};
+
+    do {
+        isNotOk = true;
+        if ((cin >> n).fail() or n < 1 or n > 10) {
+            fixStreamState();
+            cout << "Неверное значение!" << endl;
+            cout << "Введите ID: " << endl;
+        }else{
+            for (Student student : Students)
+            {
+                if (student.id == n)
+                {
+                    isNotOk = false;
+                    break;
+                }
+            }
+        }
+    } while (isNotOk);
+
+    return n;
+}
+char getCorrectChar()
+{
+    char n{};
+    bool isNotOk{};
+    do {
+        isNotOk = false;
+        if ((cin >> n).fail() or (n != 'y' and n != 'Y' and n != 'n' and n != 'N')) {
+            fixStreamState();
+            cout << "Неверное значение!" << endl;
+            cout << "Повторите попытку: " << endl;
+            isNotOk = true;
+        }
+    } while (isNotOk);
+    return n;
+}
+string getCorrectFIO()
+{
+    string n{};
+    bool isNotOk{};
+    do {
+        isNotOk = false;
+        cin >> n;
+        regex pattern("[^A-za-zА-яа-я-']");
+        if (n.length() < 2 or regex_search(n, pattern)) {
+            fixStreamState();
+            cout << "Неверное значение!" << endl;
+            cout << "Повторите попытку: " << endl;
+            isNotOk = true;
+        }
+    } while (isNotOk);
+    return n;
+}
+string getCorrectGroup()
+{
+    string n{};
+    bool isNotOk{};
+    do {
+        isNotOk = false;
+        cin >> n;
+        regex pattern("[^A-ZА-Я1-9-]");
+        if (n.length() < 2 or regex_search(n, pattern)) {
+            fixStreamState();
+            cout << "Неверное значение!" << endl;
+            cout << "Повторите попытку: " << endl;
+            isNotOk = true;
+        }
+    } while (isNotOk);
+    return n;
+}
+void studentsWithBadMarksChek()
+{
+    bool arrayIsEmpty = FreeId.size() == 10;
+    if (arrayIsEmpty)
+    {
+        cout << "База пуста" << endl;
+        return;
+    }
+    vector<int> idStudentsWithBadMarks{};
+    for (Student student : Students)
+    {
+        if (student.id == 0) continue;
+        int countTwo{};
+        for (int i = 0; i < 5; i++)
+        {
+            if (student.marks[i] == 2) countTwo++;
+        }
+        if (countTwo >= 3) idStudentsWithBadMarks.push_back(student.id);
+    }
+    if (idStudentsWithBadMarks.empty())
+    {
+        cout << "В базе нет студентов с двойками по трем и более предметам" << endl;
+        return;
+    }
+    printStudentsWithBadMarks(idStudentsWithBadMarks);
+}
+
+
